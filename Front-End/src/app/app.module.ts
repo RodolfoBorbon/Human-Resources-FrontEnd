16f782795d6f77cd99d3b742bdd9fa3a306af0a8
Rodolfo Borbon <122333787+RodolfoBorbon@users.noreply.github.com>
2023-08-06 16:00:22 -0400

    app-modules.ts set

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -3,32 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DepartmentsComponent } from './departments/departments.component';
 import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentsComponent } from './departments/departments.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { LoginComponent } from './login/login.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentsComponent,
     EmployeeComponent,
+    DepartmentsComponent,
     FooterComponent,
     HomeComponent,
     JobsComponent,
     LoginComponent,
-    NavbarComponent,
-    UserRegistrationComponent
+    UserRegistrationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
